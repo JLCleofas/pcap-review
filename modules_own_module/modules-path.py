5ecb1770f21b9c72c7ccc3fab3806b3267bf3752
John Louie Cleofas <164037152+JLCleofas@users.noreply.github.com>
2025-09-22 09:36:06 +0800

    9-20-2025 Review.

--- modules_own_module/modules-path.py
+++ modules_own_module/modules-path.py
@@ -1,9 +1,9 @@
+import secret_module
 import sys
 
 # modifying the path for secret folder
 sys.path.append('secret')
 
-import secret_module
 
 address = 'C:\\Users\\user\\..'
 address_for_outside = '..\\secret'
